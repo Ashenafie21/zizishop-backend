101b1161bfad1b98c96cd7e3ea6fd64c9afa16e0
Ashenafie21 <138523158+Ashenafie21@users.noreply.github.com>
2024-01-12 17:43:37 -0500

    index update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5500;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
